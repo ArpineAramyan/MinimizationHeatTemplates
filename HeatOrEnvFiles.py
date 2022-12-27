2104de19785a2ac08bb7f7c01783ff75dd685702
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2022-12-27 16:25:28 +0400

    separation of function heat_or_env

--- HeatOrEnvFiles.py
+++ HeatOrEnvFiles.py
@@ -30,64 +30,75 @@ def for_plan_env(eh_file, list_of_files, dict_of_services_and_files):
         if key == 'environments':
             for i in value:
                 if i.get('path'):
-                    absvalue = norm_path(eh_path[0], i.get('path'))
-                    if os.path.isfile(absvalue) and absvalue not in list_of_files and absvalue not in dict_of_services_and_files.values():
-                        #print(env_path[1]) тут только plan-env
-                        list_of_files.append(absvalue) 
+                    abs_value = norm_path(eh_path[0], i.get('path'))
+                    if os.path.isfile(abs_value) and abs_value not in list_of_files and abs_value not in dict_of_services_and_files.values():
+                        list_of_files.append(abs_value)
 
-def env_files_helper(dict_env, env_path, all_services, list_of_files, dict_of_services_and_files):
-    for key, value in dict_env.items():
-        re_match = re.match(key_env, key)
-        if re_match and isinstance(value, str):
-            if value == 'None':
-                dict_of_services_and_files.pop(key, 1)
-            else:
-                absvalue = norm_path(env_path[0], value)
-                if os.path.isfile(absvalue) and absvalue not in list_of_files and absvalue not in dict_of_services_and_files.values() and key.split()[-1] in all_services:
-                    dict_of_services_and_files.update({key: absvalue}) 
-
-def heat_files_helper(dict_heat, heat_path, list_of_files, dict_of_services_and_files):
-    for key, value in dict_heat.items():
-        re_match = re.match(key_words, key)
-        if re_match and isinstance(value, str):
-            absvalue = norm_path(heat_path[0], value) 
-            if os.path.isfile(absvalue) and absvalue not in list_of_files and absvalue not in dict_of_services_and_files.values():
-                list_of_files.append(absvalue)  
-                
-def heat_or_env(eh_file, all_services, list_of_files, dict_of_services_and_files):
+def env_files_helper(eh_file,  all_services, list_of_files, dict_of_services_and_files):
+    a = 0
     is_yaml = re.match(y_f, eh_file)
     if is_yaml:
         with open(eh_file, 'r') as fd:
             eh = yaml.load(fd, Loader=yaml.Loader)
 
-        eh_path = os.path.split(eh_file)
-
+        env_path = os.path.split(eh_file)
         if isinstance(eh, dict):
-            eh_dict = flatdict.FlatDict(eh, delimiter=' - ')
-            for key, value in eh_dict.items():
+            dict_env = flatdict.FlatDict(eh, delimiter=' - ')
+            for key, value in dict_env.items():
                 is_heat = re.match(h_f, key)
                 if is_heat:
-                    heat_files_helper(eh_dict, eh_path, list_of_files, dict_of_services_and_files)
+                    a = 1
                     break
-            else:
-                env_files_helper(eh_dict, eh_path, all_services, list_of_files, dict_of_services_and_files)
+            if a == 0:
+                for key, value in dict_env.items():
+                    re_match = re.match(key_env, key)
+                    if re_match and isinstance(value, str):
+                        if value == 'None':
+                            dict_of_services_and_files.pop(key.split()[-1], 1)
+                        else:
+                            abs_value = norm_path(env_path[0], value)
+                            if os.path.isfile(abs_value) and abs_value not in list_of_files and abs_value not in dict_of_services_and_files.values() and key.split()[-1] in all_services:
+                                dict_of_services_and_files.update({key.split()[-1]: abs_value})
+
+def heat_files_helper(eh_file, list_of_files, dict_of_services_and_files):
+    a = 0
+    is_yaml = re.match(y_f, eh_file)
+    if is_yaml:
+        with open(eh_file, 'r') as fd:
+            eh = yaml.load(fd, Loader=yaml.Loader)
+        heat_path = os.path.split(eh_file)
+        
+        if isinstance(eh, dict):
+            dict_heat = flatdict.FlatDict(eh, delimiter=' - ')
+            for key, value in dict_heat.items():
+                is_heat = re.match(h_f, key)
+                if is_heat:
+                    a = 1
+            if a == 1:
+                for key, value in dict_heat.items():
+                    re_match = re.match(key_words, key)
+                    if re_match and isinstance(value, str):
+                        abs_value = norm_path(heat_path[0], value)
+                        if os.path.isfile(abs_value) and abs_value not in list_of_files and abs_value not in dict_of_services_and_files.values():
+                            list_of_files.append(abs_value)
 
 def recursive_list(r_list, index, all_services, list_of_files, dict_of_services_and_files):
     if len(r_list) <= index:
         return    
-    heat_or_env(r_list[index], all_services, list_of_files, dict_of_services_and_files)
+    env_files_helper(r_list[index], all_services, list_of_files, dict_of_services_and_files)
     recursive_list(list_of_files, index + 1, all_services, list_of_files, dict_of_services_and_files)
     
-def recursive_dict(r_dict, index, all_services, list_of_files, dict_of_services_and_files):
+def recursive_dict(r_dict, index, list_of_files, dict_of_services_and_files):
     if len(r_dict) <= index:
         return
-    heat_or_env(r_dict[index][1], all_services, list_of_files, dict_of_services_and_files)
-    recursive_dict(list(dict_of_services_and_files.items()), index + 1, all_services, list_of_files, dict_of_services_and_files)
+    heat_files_helper(r_dict[index][1], list_of_files, dict_of_services_and_files)
+    recursive_dict(list(dict_of_services_and_files.items()), index + 1, list_of_files, dict_of_services_and_files)
                 
 def main(roles_data, overcloud, overcloud_resource_registry_puppet, plan_environment): 
     list_of_files = []
     dict_of_services_and_files = {}
     
+    list_of_files.append(roles_data)
     list_of_files.append(overcloud)
     list_of_files.append(overcloud_resource_registry_puppet)
     list_of_files.append(plan_environment)
@@ -95,7 +106,7 @@ def main(roles_data, overcloud, overcloud_resource_registry_puppet, plan_environ
     all_services = default_services(roles_data)
     for_plan_env(plan_environment, list_of_files, dict_of_services_and_files)
     recursive_list(list_of_files, 0, all_services, list_of_files, dict_of_services_and_files)
-    recursive_dict(list(dict_of_services_and_files.items()), 0, all_services, list_of_files, dict_of_services_and_files)
+    recursive_dict(list(dict_of_services_and_files.items()), 0, list_of_files, dict_of_services_and_files)
 
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
