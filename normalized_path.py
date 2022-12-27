b6f7745250dabf264bb1e3045ab3d811b671c7f3
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2022-12-27 16:15:08 +0400

    function changed

--- normalized_path.py
+++ normalized_path.py
@@ -13,12 +13,9 @@ def norm_path(path_global, file_path):
             ret = os.path.join(p2, file_path[3:])
         elif re_match2:
             ret = os.path.join(path_global, file_path[2:])
+        re_match1 = re.match('^../', file_path[3:])
+        if re_match1:
+            ret = norm_path(p2, file_path[3:])        
     else:
         ret = os.path.join(path_global, file_path)
-
-    re_match1 = re.match('^../', file_path[3:])
-
-    if re_match1:
-        ret = norm_path(p2, file_path[3:])
-
     return ret      
