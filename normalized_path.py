caa5c23439a1a45f8ce13710603f14382a6bd436
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2023-01-05 17:49:24 +0400

    function name changed

--- normalized_path.py
+++ normalized_path.py
@@ -1,11 +1,11 @@
 import re
 import os.path
 
-def norm_path(path_global, file_path):
+
+def normalized_path(path_global, file_path):
     ret = ''
     re_match1 = re.match('^../', file_path)
     re_match2 = re.match('^./', file_path)
-
     if re_match1 or re_match2:
         p1 = os.path.split(path_global)
         p2 = p1[0]
@@ -15,7 +15,7 @@ def norm_path(path_global, file_path):
             ret = os.path.join(path_global, file_path[2:])
         re_match1 = re.match('^../', file_path[3:])
         if re_match1:
-            ret = norm_path(p2, file_path[3:])        
+            ret = normalized_path(p2, file_path[3:])
     else:
         ret = os.path.join(path_global, file_path)
-    return ret      
+    return ret
