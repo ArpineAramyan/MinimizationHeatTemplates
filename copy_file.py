299c3860119bb66f6b88341c582faede476ae514
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2023-01-24 15:03:54 +0400

    variables renamed

--- copy_file.py
+++ copy_file.py
@@ -4,17 +4,17 @@ import re
 import pathlib
 
 
-def copy_file(path, file, home, home_copy, path_parts, home_at_first, home_copy_at_first):
-    file_path = os.path.join(home_at_first, path, file)
-    new_file_path = os.path.join(home_copy_at_first, path, file)
+def copy_file(path, file, temporary_home, temporary_home_copy, path_parts, home, home_copy):
+    file_path = os.path.join(home, path, file)
+    new_file_path = os.path.join(home_copy, path, file)
     if len(path_parts) == 0:
         shutil.copyfile(file_path, new_file_path)
         return
-    if not os.path.isdir(os.path.join(home_copy, path_parts[0])):
-        os.mkdir(os.path.join(home_copy, path_parts[0]))
-        copy_file(path, file, os.path.join(home, path_parts[0]), os.path.join(home_copy, path_parts[0]),
-                   path_parts[1:], home_at_first, home_copy_at_first)
+    if not os.path.isdir(os.path.join(temporary_home_copy, path_parts[0])):
+        os.mkdir(os.path.join(temporary_home_copy, path_parts[0]))
+        copy_file(path, file, os.path.join(temporary_home, path_parts[0]),
+                  os.path.join(temporary_home_copy, path_parts[0]), path_parts[1:], home, home_copy)
     else:
-        copy_file(path, file, os.path.join(home, path_parts[0]), os.path.join(home_copy, path_parts[0]),
-                   path_parts[1:], home_at_first, home_copy_at_first)
+        copy_file(path, file, os.path.join(temporary_home, path_parts[0]),
+                  os.path.join(temporary_home_copy, path_parts[0]), path_parts[1:], home, home_copy)
     return
