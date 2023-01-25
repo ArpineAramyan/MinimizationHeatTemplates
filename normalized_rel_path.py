4b2503c4db24e898959725ee0fccf6cca2fcb1c9
ArpineAramyan <93595937+ArpineAramyan@users.noreply.github.com>
2023-01-25 11:48:24 +0400

    renamed variables and removed unnecessary

--- normalized_rel_path.py
+++ normalized_rel_path.py
@@ -3,20 +3,19 @@ import os.path
 
 
 def normalized_rel_path(path_global, file_path):
-    ret = ''
-    re_match1 = re.match('^../', file_path)
-    re_match2 = re.match('^./', file_path)
+    normalized = ''
+    previous_match = re.match('^../', file_path)
+    current_match = re.match('^./', file_path)
 
-    if re_match1 or re_match2:
-        p1 = os.path.split(path_global)
-        p2 = p1[0]
-        if re_match1:
-            ret = os.path.join(p2, file_path[3:])
-        elif re_match2:
-            ret = os.path.join(path_global, file_path[2:])
-        re_match1 = re.match('^../', file_path[3:])
-        if re_match1:
-            ret = normalized_rel_path(p2, file_path[3:])
+    if previous_match or current_match:
+        directories = os.path.split(path_global)[0]
+        if previous_match:
+            normalized = os.path.join(directories, file_path[3:])
+        elif current_match:
+            normalized = os.path.join(path_global, file_path[2:])
+        previous_match = re.match('^../', file_path[3:])
+        if previous_match:
+            normalized = normalized_rel_path(directories, file_path[3:])
     else:
-        ret = os.path.join(path_global, file_path)
-    return ret
+        normalized = os.path.join(path_global, file_path)
+    return normalized
