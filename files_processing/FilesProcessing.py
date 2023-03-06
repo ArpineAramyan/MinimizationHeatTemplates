5b46e5a9012700809060d02279a88977226e0366
ArpineAramyan <api2002@mail.ru>
2023-03-06 13:38:54 +0400

    changed setup.cfg and moved FilesProcessing.py

--- files_processing/FilesProcessing.py
+++ files_processing/FilesProcessing.py
@@ -165,11 +165,6 @@ def heat_files_traversal(index, extra_files, services_and_files, hot_home, file_
                          file_resources)
     heat_files_traversal(index + 1, extra_files, services_and_files, hot_home, file_resources)
 
-# ='./'
-# ='../minimized-heat-templates'
-# ='roles_data.yaml'
-# ='plan-environment.yaml'
-
 
 def main(hot_home,  copy_hot_home, roles_data_path, plan_env_path):
     if not os.path.isdir(hot_home):
@@ -210,8 +205,6 @@ def main(hot_home,  copy_hot_home, roles_data_path, plan_env_path):
         path_parts = pathlib.PosixPath(path)
         path_parts = list(path_parts.parts)
         copy_file(path, file, path_parts, hot_home, copy_hot_home)
-
-    # print(len(extra_files) + len(services_and_files) + len(file_resources))
 
 
 if __name__ == "__main__":
